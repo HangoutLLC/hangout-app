69db45da664ec261ccdd33d98797f4771f825680
SeanHingco <seanescan@gmail.com>
2025-07-16 16:50:19 -0700

    chore(mobile): added Sizes sheet for font and layout conventions

---
+++ mobile/constants/Sizes.ts
@@ -0,0 +1,40 @@
+const sizes = {
+    spacing: {
+      xs: 4,
+      sm: 8,
+      md: 16,
+      lg: 24,
+      xl: 32,
+      xxl: 48,
+    },
+    font: {
+      small: 12,
+      medium: 16,
+      large: 20,
+      xl: 24,
+      xxl: 32,
+    },
+    radii: {
+      sm: 4,
+      md: 8,
+      lg: 16,
+      pill: 999,
+    },
+    layout: {
+      headerHeight: 60,
+      footerHeight: 50,
+      tabBarHeight: 56,
+    },
+    icon: {
+      sm: 16,
+      md: 24,
+      lg: 32,
+    },
+    breakpoints: {
+      phone: 0,
+      tablet: 768,
+      desktop: 1024,
+    },
+  };
+  
+  export default sizes;
