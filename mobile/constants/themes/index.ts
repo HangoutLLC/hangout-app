6fb541198ffdfffe614288f295f62aeeccc44c79
SeanHingco <seanescan@gmail.com>
2025-08-14 14:45:04 -0700

    feat(mobile): added scaffolding for ThemeProvider

---
+++ mobile/constants/themes/index.ts
@@ -0,0 +1,21 @@
+// theme/index.ts
+import { createTheme } from '@rneui/themed';
+import { lightTheme, darkTheme } from './colors';
+
+export function buildAppTheme(mode: 'light' | 'dark') {
+  return createTheme({
+    mode,
+    lightColors: lightTheme,
+    darkColors: darkTheme,
+    components: {
+        Button: {
+            raised: true,
+            buttonStyle: { borderRadius: 12 },
+            titleStyle: { fontWeight: '700' },
+        },
+        Text: {
+            h1Style: { fontSize: 28, fontWeight: '800' },
+        },
+    },
+  });
+}
