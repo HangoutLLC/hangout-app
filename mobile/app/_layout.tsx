044cb55d2b0540535cec477ece3a1d089ae18d30
Kevin Ye <kevinye91@gmail.com>
2025-08-28 16:23:49 -0700

    feat: create authorization provider and wrapper for handling user session and user login redirect respectively

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,30 +1,36 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-import { ThemeProvider as RNEThemeProvider } from '@rneui/themed';
-import { buildAppTheme } from '@/constants/themes';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import { ThemeProvider as RNEThemeProvider } from "@rneui/themed";
+import { buildAppTheme } from "@/constants/themes";
+import { useColorScheme } from "@/hooks/useColorScheme";
 // import {
 //   LibertinusSerif_400Regular,
 //   LibertinusSerif_600SemiBold,
 //   LibertinusSerif_700Bold,
 // } from '@expo-google-fonts/libertinus-serif';
-import { PaperProvider } from 'react-native-paper';
+import { PaperProvider } from "react-native-paper";
 
+import { AuthProvider } from "@/components/AuthProvider";
+import Auth from "@/components/Auth";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const mode = colorScheme === 'dark' ? 'dark' : 'light';
+  const mode = colorScheme === "dark" ? "dark" : "light";
   const appTheme = buildAppTheme(mode);
-  const navTheme = colorScheme === 'dark' ? DarkTheme : DefaultTheme;
+  const navTheme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     // LibertinusSerif_400Regular,
     // LibertinusSerif_600SemiBold,
     // LibertinusSerif_700Bold,
-  })
+  });
 
   if (!loaded) {
     // Async font loading only occurs in development.
@@ -32,18 +38,19 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={navTheme}>
-      <RNEThemeProvider
-        theme={appTheme}
-      >
-        <PaperProvider>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-          <StatusBar style="auto" />
-        </PaperProvider>
-      </RNEThemeProvider>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider value={navTheme}>
+        <RNEThemeProvider theme={appTheme}>
+          <PaperProvider>
+            <Stack initialRouteName="login">
+              <Stack.Screen name="login" options={{ headerShown: false }} />
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="+not-found" />
+            </Stack>
+            <StatusBar style="auto" />
+          </PaperProvider>
+        </RNEThemeProvider>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
