0518263d567f543b306f679dd900c7638741a005
RyanAra <ryanrocks123467@gmail.com>
2025-09-12 18:08:31 -0700

    feat (mobile): added alert, and page, modfied previous files to account for new additions

--- mobile/components/HomescreenHeader.tsx
+++ mobile/components/HomescreenHeader.tsx
@@ -4,12 +4,12 @@ import { Ionicons } from '@expo/vector-icons';
 
 export default function HomescreenHeader() {
   return (
-    <View /*style={styles.container}*/>
-        <View /*style={styles.row}*/>
-            <Text /*style={styles.title}*/>Welcome Back!</Text>
+    <View style={styles.container}>
+        <View style={styles.row}>
+            <Text style={styles.title}>Welcome Back!</Text>
             <Image
                 source={require('../assets/images/favicon.png')}
-                /*style={styles.logo}*/
+                style={styles.logo}
                 resizeMode="contain"
             />
         </View>
@@ -17,27 +17,27 @@ export default function HomescreenHeader() {
   );
 }
 
-// const styles = StyleSheet.create({
-//     container: {
-//         alignItems: 'center',
-//         paddingHorizontal: 16,
-//         paddingVertical: 12,
-//         backgroundColor: '#fff',
-//     },
-//     row: {
-//         flexDirection: 'row',
-//         alignItems: 'center',
-//         justifyContent: 'space-between',
-//         gap: 8,
-//     },
-//     title: {
-//         fontSize: 24,
-//         fontWeight: 'bold',
-//         color: '#222',
-//     },
-//     logo: {
-//         width: 40,
-//         height: 40,
-//         resizeMode: 'contain',
-//     }
-// })
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        paddingHorizontal: 16,
+        paddingVertical: 12,
+        backgroundColor: '#fff',
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 8,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#222',
+    },
+    logo: {
+        width: 40,
+        height: 40,
+        resizeMode: 'contain',
+    }
+})
